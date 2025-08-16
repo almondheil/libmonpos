024822053e5c2ed88205483cd4bdb509c6171c68
almond Heil <almond@almendra.dev>
2025-08-16 09:50:23 -0600

    Change return values of ReadConfig, define monitor string func

--- libmonpos.go
+++ libmonpos.go
@@ -1,6 +1,7 @@
 package libmonpos
 
 import (
+	"fmt"
 	"os"
 
 	"gopkg.in/yaml.v3"
@@ -10,8 +11,8 @@ import (
 ///
 /// The width, height, and scale keys are required, and the position and align are optional.
 type Monitor struct {
-	Width int
-	Height int
+	Width uint
+	Height uint
 	Scale float32
 	Position string `yaml:",omitempty"`
 	Align string `yaml:",omitempty"`
@@ -22,28 +23,38 @@ type Config struct {
 	Monitors map[string]Monitor
 }
 
+func (m Monitor) String() string {
+	dimensions := fmt.Sprintf("monitor %dx%d @ %.2fx", m.Width, m.Height, m.Scale)
+	if m.Position == "" {
+		return dimensions
+	}
+
+	position := fmt.Sprintf("%v, align %v", m.Position, m.Align)
+	return dimensions + " " + position
+}
+
 /// Read a config file from disk and ensure that it is valid.
 ///
 /// If position has a value but align is not specified, it will be defaulted to "center"
-func ReadConfig(path string) (error, Config) {
+func ReadConfig(path string) (Config, error) {
 	// read the file from the system
 	data, err := os.ReadFile(path)
 	if err != nil {
-		return err, Config{}
+		return Config{}, err
 	}
 
 	// unmarshal from yaml into the correct structure
 	conf := Config{}
 	err = yaml.Unmarshal([]byte(data), &conf)
 	if err != nil {
-		return err, Config{}
+		return Config{}, err
 	}
 
 	// validate the config before returning it
 	err = validate_config(conf)
 	if err != nil {
-		return err, Config{} 
+		return Config{}, err
 	}
 
-	return nil, conf
+	return conf, nil
 }
