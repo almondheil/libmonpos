78ae6d93203ff4abb5b53761569582a21dddd1ea
almond Heil <almond@almendra.dev>
2025-08-15 22:36:32 -0600

    Delete types.go, merge into main library file

--- libmonpos.go
+++ libmonpos.go
@@ -6,6 +6,22 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+/// One monitor in the config file.
+///
+/// The width, height, and scale keys are required, and the position and align are optional.
+type Monitor struct {
+	Width int
+	Height int
+	Scale float32
+	Position string `yaml:",omitempty"`
+	Align string `yaml:",omitempty"`
+}
+
+/// The entire config file is made up of multiple Monitor entries, under a common monitors header.
+type Config struct {
+	Monitors map[string]Monitor
+}
+
 /// Read a config file from disk and ensure that it is valid.
 ///
 /// If position has a value but align is not specified, it will be defaulted to "center"
