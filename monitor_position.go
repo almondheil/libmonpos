c4244680c100893267653e86a9ce200115a28dd2
almond Heil <almond@almendra.dev>
2025-08-15 21:28:39 -0600

    Implement config validation

--- monitor_position.go
+++ monitor_position.go
@@ -3,13 +3,24 @@ package main // monitor_position
 import (
 	"fmt"
 	"os"
+	"slices"
+	"strings"
 
-	"gopkg.in/yaml.v3"											
+	"gopkg.in/yaml.v3"
 )
+
+/// Valid directions
+var directions = []string{"left-of", "right-of", "above", "below"}
+var directions_horiz = []string{"left-of", "right-of"}
+
+/// Valid alignments
+var alignments_horiz = []string{"center", "left", "right"}
+var alignments_vert = []string{"center", "top", "bottom"}
+
 
 /// One monitor in the config file.
 ///
-/// The width, height, and scale keys are required, and the position and align are optional.
+/// The `width`, `height`, and `scale` keys are required, and the `position` and `align` are optional.
 type Monitor struct {
 	Width int
 	Height int
@@ -18,28 +29,94 @@ type Monitor struct {
 	Align string `yaml:",omitempty"`
 }
 
-/// The entire config file is made up of multiple Monitor entries, under a common Monitor header.
+/// The entire config file is made up of multiple Monitor entries, under a common `monitors` header.
 type Config struct {
 	Monitors map[string]Monitor
 }
 
+/// Read a config file from disk and ensure that it is valid.
 func read_config(path string) Config {
+	// read the file from the system
 	data, err := os.ReadFile(path)
 	if err != nil {
 		panic(err)
 	}
 
+	// unmarshal from yaml into the correct structure
 	conf := Config{}
 	err = yaml.Unmarshal([]byte(data), &conf)
 	if err != nil {
 		panic(err)
 	}
 
+	// validate the config before returning it
+	validate_config(conf)
 	return conf
 }
 
+/// Validate a monitor config, ensuring that the position and align are valid if they are defined.
+func validate_monitor(m Monitor, monitor_names []string) error {
+	if m.Position == "" && m.Align == "" {
+		return nil // position and align may both be empty
+	} else if m.Position == "" && m.Align != "" {
+		return fmt.Errorf("position is blank, so alignment must also be blank")
+	}
+
+	// check the general format of position, <word> <word>
+	parts := strings.Split(m.Position, " ")
+	if len(parts) != 2 {
+		return fmt.Errorf("position must be of the form <direction> <monitor>")
+	}
+
+	// make sure the first and second word are actually a direction and name respectively
+	if !slices.Contains(directions, parts[0]) {
+		return fmt.Errorf("expected direction `above`, `below`, `left-of`, or `right-of`, got `%v`", parts[0]) 
+	}
+	if !slices.Contains(monitor_names, parts[1]) {
+		return fmt.Errorf("expected a monitor name, got %v", parts[1])
+	}
+
+	// if m.Align is empty, now we can set it to the always-valid value "center"
+	if m.Align == "" {
+		m.Align = "center"
+		return nil
+	}
+
+	// depending on if the direction is horizontal, decide whether the alignment is valid or not
+	is_horiz := slices.Contains(directions_horiz, parts[0])
+	if is_horiz && !slices.Contains(alignments_vert, m.Align) {
+		return fmt.Errorf("for horizontal alignments, only alignments `top`, `bottom`, and `center` are valid. got `%v`", m.Align)
+	} else if !slices.Contains(alignments_horiz, m.Align) {
+		return fmt.Errorf("for vertical alignments, only alignments `left`, `right,` and `center` are valid. got `%v`", m.Align)
+	}
+
+	// getting to the end with no errors means we are officially done and the monitor definition is valid
+	return nil
+}
+
+/// Validate all monitors in a config, returning an error as soon as it is detected.
+func validate_config(c Config) error {
+	monitors := c.Monitors
+
+	// put all monitor names in a list
+	monitor_names := make([]string, len(monitors))
+	for name := range monitors {
+		monitor_names = append(monitor_names, name)
+	}
+
+	// check the parameters of each monitor
+	for name, monitor := range monitors {
+		err := validate_monitor(monitor, monitor_names)
+		if err != nil {
+			return fmt.Errorf("monitor %v is invalid: %v", name, err)
+		}
+	}
+
+	return nil
+}
 
 func main() {
 	config := read_config("./example.yaml")
+
 	fmt.Printf("%v\n", config)
 }
