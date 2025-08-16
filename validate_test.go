2e5bcb13ff853e489d383b359575eedb8bbe6ca4
almond Heil <almond@almendra.dev>
2025-08-16 17:02:28 -0600

    Add tests for loading configs

--- validate_test.go
+++ validate_test.go
@@ -68,11 +68,18 @@ func TestValidateConfigFiles(t *testing.T) {
 		conf_file string
 		expected bool
 	}{
+		{"test_files/not_yaml.txt", false},
 		{"test_files/basic.yaml", true},
 		{"test_files/basic_with_yaml_header.yaml", true},
 		{"test_files/complex.yaml", true},
-		{"test_files/invalid_names.yaml", false},
-		{"test_files/not_yaml.txt", false},
+		{"test_files/missing_names.yaml", false},
+		{"test_files/cycle.yaml", false},
+		{"test_files/cycle_one.yaml", false},
+		{"test_files/two_roots.yaml", false},
+		{"test_files/unspecified_align.yaml", true},
+		{"test_files/unspecified_scale.yaml", true},
+		{"test_files/unspecified_width.yaml", false},
+		{"test_files/unspecified_height.yaml", false},
 	}
 
 	for _, tt := range tests {
@@ -93,22 +100,19 @@ func TestValidateConfigFiles(t *testing.T) {
 	}
 }
 
-/*
-func TestOrder(t *testing T) {
-}
-
-func TestDefaultAlign(t *testing.T) {
+func TestDefaults(t *testing.T) {
 	tests := []struct{
 		conf_file string
 		name string
 		expected Monitor
 	}{
-		{"test_files/default_align.yaml", "secondary", Monitor{1920, 1080, 2.0, "left-of primary", "center"}},
+		{"test_files/unspecified_scale.yaml", "B", Monitor{20, 20, 1.0, "below A", "right"}},
+		{"test_files/unspecified_align.yaml", "B", Monitor{20, 20, 2.0, "below A", "center"}},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.conf_file, func (t *testing.T) {
-			conf, err := LoadConfig(tt.conf_file)
+			conf, _, err := LoadConfig(tt.conf_file)
 			if err != nil {
 				t.Errorf("unexpected error reading config: %v", err)
 				return
@@ -121,4 +125,11 @@ func TestDefaultAlign(t *testing.T) {
 		})
 	}
 }
+
+/*
+// confirm the monitor order is not contradictory ever
+func TestMonitorOrder(t *testing.T) {
+
+}
 */
+
